422ab675e337113de48c45cfd3d928465e36bfcb
nvietto <nvietto@unomaha.edu>
2023-08-02 11:50:12 -0500

    update

--- R/movienight.R
+++ R/movienight.R
@@ -1,3 +1,13 @@
+#' Recommends a movie
+#'
+#' @param x Leave the argument empty
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' movienight()
+#' [1] "Up"
 movienight <- function(x) {
   random_index <- sample(1:nrow(data), 1)
   movie <- data[random_index, ]
